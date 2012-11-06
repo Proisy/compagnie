8cbfe64989409b295133f67e88119b2244b4cfba
Killian BLAIS <killian.blais@orange.fr>
2012-11-06 16:38:44 +0100

    Fonctionnalités de création et modification de modèles

--- application/controllers/ModeleController.php
+++ application/controllers/ModeleController.php
@@ -12,16 +12,48 @@ class ModeleController extends Zend_Controller_Action
 	public function ajouterAction() {
 		$form = new Zend_Form;
 
+		$form->setAction('/modele/ajouter/')->setMethod('post');
+
+		$listeInput['modele_marque'] = new Zend_Form_Element_Text('modele_marque');
+		$listeInput['modele_reference'] = new Zend_Form_Element_Text('modele_reference');
+		$listeInput['modele_rayon'] = new Zend_Form_Element_Text('modele_rayon');
+		$listeInput['modele_piste_att'] = new Zend_Form_Element_Text('modele_piste_att');
+		$listeInput['modele_piste_dec'] = new Zend_Form_Element_Text('modele_piste_dec');
+		$listeInput['modele_nb_passagers'] = new Zend_Form_Element_Text('modele_nb_passagers');
+		$listeInput['modele_diff_revision'] = new Zend_Form_Element_Text('modele_diff_revision');
+
+		$listeInput['modele_marque']->setLabel('Marque')
+									->addValidator(new Zend_Validate_Digits());
+		$listeInput['modele_reference']->setLabel('Référence')
+									->addValidator(new Zend_Validate_Digits());
+		$listeInput['modele_rayon']->setLabel('Rayon (en km)')
+									->addValidator(new Zend_Validate_Digits());
+		$listeInput['modele_piste_att']->setLabel('Longueur nécessaire pour l\'atterissage')
+									->addValidator(new Zend_Validate_Digits());
+		$listeInput['modele_piste_dec']->setLabel('Longueur nécessaire pour le décollage')
+									->addValidator(new Zend_Validate_Digits());
+		$listeInput['modele_nb_passagers']->setLabel('Nombre de passagers')
+									->addValidator(new Zend_Validate_Digits());
+		$listeInput['modele_diff_revision']->setLabel('Durée entre deux révisions')
+									->addValidator(new Zend_Validate_Digits());
+
+		foreach ($listeInput as $key=>$value) {
+			$value->setRequired(true);
+			$form->addElement($value);
+		}
+
+		$form->addElement(new Zend_Form_Element_Submit('Valider'));
+
 		if($this->getRequest()->isPost()) {
 			$post = $this->getRequest()->getPost();
 			$form->isValid($post);
-			$data = $monForm->getValues();
+			$data = $form->getValues();
 
 			$tableModele = new TModele;
 			$tableModele->addModele($data);
 		}
 		else {
-			echo $form;
+			$this->view->form = $form;
 		}
 	}
 
@@ -30,17 +62,49 @@ class ModeleController extends Zend_Controller_Action
 		$tableModele = new TModele;
 
 		$form = new Zend_Form;
+
+		$form->setAction('/modele/modifier/')->setMethod('post');
+
+		$listeInput['modele_marque'] = new Zend_Form_Element_Text('modele_marque');
+		$listeInput['modele_reference'] = new Zend_Form_Element_Text('modele_reference');
+		$listeInput['modele_rayon'] = new Zend_Form_Element_Text('modele_rayon');
+		$listeInput['modele_piste_att'] = new Zend_Form_Element_Text('modele_piste_att');
+		$listeInput['modele_piste_dec'] = new Zend_Form_Element_Text('modele_piste_dec');
+		$listeInput['modele_nb_passagers'] = new Zend_Form_Element_Text('modele_nb_passagers');
+		$listeInput['modele_diff_revision'] = new Zend_Form_Element_Text('modele_diff_revision');
+
+		$listeInput['modele_marque']->setLabel('Marque')
+									->addValidator(new Zend_Validate_Digits());
+		$listeInput['modele_reference']->setLabel('Référence')
+									->addValidator(new Zend_Validate_Digits());
+		$listeInput['modele_rayon']->setLabel('Rayon (en km)')
+									->addValidator(new Zend_Validate_Digits());
+		$listeInput['modele_piste_att']->setLabel('Longueur nécessaire pour l\'atterissage')
+									->addValidator(new Zend_Validate_Digits());
+		$listeInput['modele_piste_dec']->setLabel('Longueur nécessaire pour le décollage')
+									->addValidator(new Zend_Validate_Digits());
+		$listeInput['modele_nb_passagers']->setLabel('Nombre de passagers')
+									->addValidator(new Zend_Validate_Digits());
+		$listeInput['modele_diff_revision']->setLabel('Durée entre deux révisions')
+									->addValidator(new Zend_Validate_Digits());
+		$dataOld = $tableModele->getModele($id);
+
+		foreach ($listeInput as $key=>$value) {
+			$value->setRequired(true)->setValue($dataOld[$key]);
+			$form->addElement($value);
+		}
+
+		$form->addElement(new Zend_Form_Element_Submit('Valider'));
 		
 		if($this->getRequest()->isPost()) {
 			$post = $this->getRequest()->getPost();
 			$form->isValid($post);
-			$data = $monForm->getValues();
+			$data = $form->getValues();
 
 			$tableModele->editModele($id,$data);
 		}
 		else {
-			$this->view->data = $tableModele->getModele($id);
-			echo $form;
+			$this->view->form = $form;
 		}
 	}
 
