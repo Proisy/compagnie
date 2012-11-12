5ab5f99230860ed55039c479ab27c467ac69b6e6
Killian BLAIS <killian.blais@orange.fr>
2012-11-12 18:53:42 +0100

    mise en place de la base de l'API JSON

---
+++ application/controllers/ApiController.php
@@ -0,0 +1,67 @@
+<?php
+
+class ApiController extends Zend_Controller_Action
+{
+	public function init(){
+		$this->_helper->layout->disableLayout();
+		header('Content-type: application/json');
+		$this->fonction = $this->getRequest()->getParam('fonction');
+		$this->listeOperator = array("!=",">=","<=","=",">","<");
+	}
+
+	public function indexAction(){
+		$obj = null;
+		$obj->msg = "Rien à foutre ici";
+		echo json_encode($obj);
+	}
+
+	public function avionAction() {
+		switch ($this->fonction) {
+			case 'get':
+				$id = $this->getRequest()->getParam('id');
+				$tableAvion = new TAvion;
+				echo json_encode($tableAvion->getAvion($id));
+				break;
+			case 'getAll':
+				$tableAvion = new TAvion;
+				$columnsStr = $this->getRequest()->getParam('columns');
+				$filterStr = $this->getRequest()->getParam('filter');
+				$sortStr = $this->getRequest()->getParam('sort');
+
+				$filter = array();
+				if(isset($filterStr)){
+					$filterStr = explode(' ', $filterStr);
+					foreach ($filterStr as $string) {
+						foreach ($this->listeOperator as $operator) {
+							if(strstr($string,$operator)!=false) {
+								$tmpArr = explode($operator, $string);
+								$filter[] = array(
+									'column' => $tmpArr[0],
+									'operator' => $operator,
+									'value' => $tmpArr[1]
+								);
+								break;
+							}
+						}
+					}
+				}
+
+				if(isset($columnsStr)){ $columns = explode(' ',$columnsStr); }
+				else { $columns = '*'; }
+
+				if(!empty($filter)){
+					echo json_encode($tableAvion->getAvionsBy($filter, $columns));
+				}
+				else {
+					echo json_encode($tableAvion->getAllAvions($columns));
+				}
+				break;
+
+			default:
+				$obj->msg = "Liste des fonctions disponibles";
+				$obj->liste = array('get','getAll');
+				echo json_encode($obj);
+				break;
+		}
+	}
+}
